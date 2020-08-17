1399b0ff1f49831e697b0127644e25a272be92b1
Emile Cyimena <cyimena09@hotmail.com>
2020-08-17 21:08:14 +0200

    first version of my project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeViewComponent} from "./home-view/home-view.component";
+import {LoginViewComponent} from "./login-view/login-view.component";
+import {RegisterViewComponent} from "./register-view/register-view.component";
+import {NotFoundViewComponent} from "./not-found-view/not-found-view.component";
+import {AdminSpaceViewComponent} from "./admin-space-view/admin-space-view.component";
+import {AuthGuardService} from "./services/auth-guard/auth-guard.service";
+import {AddProjectViewComponent} from "./add-project-view/add-project-view.component";
+import {ProjectDetailComponent} from "./projects/project-detail/project-detail.component";
+import {UserSpaceViewComponent} from "./user-space-view/user-space-view.component";
+import {TransactionHistoricComponent} from "./transaction-historic/transaction-historic.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeViewComponent},
+  {path: 'project_detail/:id', component: ProjectDetailComponent},
+  {path: 'login', component: LoginViewComponent},
+  {path: 'register', component: RegisterViewComponent},
+  {path: 'add_project', canActivate: [AuthGuardService], component: AddProjectViewComponent},
+  {path : 'user_space', canActivate: [AuthGuardService], component: UserSpaceViewComponent},
+  {path: 'admin_space', canActivate: [AuthGuardService], component: AdminSpaceViewComponent},
+  {path: 'historic_transaction', component: TransactionHistoricComponent},
+  {path: 'not_found', component: NotFoundViewComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'not_found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { HomeViewComponent } from './home-view/home-view.component';
+import { NavViewComponent } from './nav-view/nav-view.component';
+import { LoginViewComponent } from './login-view/login-view.component';
+import { RegisterViewComponent } from './register-view/register-view.component';
+import { NotFoundViewComponent } from './not-found-view/not-found-view.component';
+import { AdminSpaceViewComponent } from './admin-space-view/admin-space-view.component';
+import { UserSpaceViewComponent } from './user-space-view/user-space-view.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddProjectViewComponent } from './add-project-view/add-project-view.component';
+import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
+import { TransactionHistoricComponent } from './transaction-historic/transaction-historic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    NavViewComponent,
+    LoginViewComponent,
+    RegisterViewComponent,
+    NotFoundViewComponent,
+    AdminSpaceViewComponent,
+    AddProjectViewComponent,
+    ProjectDetailComponent,
+    UserSpaceViewComponent,
+    TransactionHistoricComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
